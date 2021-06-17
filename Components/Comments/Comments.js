05ef2308207ce3a0bce0b33b40e82470222bafc5
tutusT <turab.guseynov@gmail.com>
2021-06-17 15:24:03 +0400

    Update Comments.js

--- Components/Comments/Comments.js
+++ Components/Comments/Comments.js
@@ -1,15 +1,13 @@
 import Comment from '../Comment'
 
-const Comments = ({ comments }) => {
+const Comments = ({ comments, state }) => {
   const renderComments = comments => {
     return comments.map(({ id, name, body }) => {
       return <Comment key={id} name={name} body={body} />
     })
   }
 
-  return (
-    <div className="comments-wrapper__items">{renderComments(comments)}</div>
-  )
+  return <div className={`comments ${state}`}>{renderComments(comments)}</div>
 }
 
 export default Comments
